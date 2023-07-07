e163f1bad2f87ae8721bf961c40ab1c5e830caa5
aurinelle <aurinelle.bouekassa-taboudi@digital.justice.gov.uk>
2023-07-07 12:00:30 +0100

    first commit

--- RobotGrid.java
+++ RobotGrid.java
@@ -2,8 +2,15 @@ public class RobotGrid {
     // Placeholder code for the main implementation class
 
     public boolean isSafe(int x, int y) {
-        // TODO: Add implementation logic here
-        throw new UnsupportedOperationException("Not implemented yet");
+        String allDigits = String.valueOf(Math.abs(x)) + String.valueOf(Math.abs(y));
+        int sum = 0;
+
+        for (int i = 0; i < allDigits.length(); i++) {
+            String c = "" + allDigits.charAt(i);
+            sum = sum + Integer.valueOf(c);
+        }
+
+        return sum > 23 ? false : true;
     }
 
     public int totalSafeSquares() {
